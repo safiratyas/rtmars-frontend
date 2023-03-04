72a75d468e1e5121326568347e7cd2a456434950
Safira Tyas Wandita <safeeratyas@gmail.com>
2023-03-04 19:29:47 +0700

    add: accordion for frequently asked questions

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import NavbarHomepage from "../components/Navbar/NavbarHome";
 import Header from "../components/Header/Header";
 import CardDescription from "../components/Cards/Description/CardDescription";
 import CardInterest from "../components/Cards/Description/CardInterest";
+import Faq from "../components/Accordion/Faq";
 import Footer from "../components/Footer/Footer";
 
 function Homepage() {
@@ -11,6 +12,7 @@ function Homepage() {
       <NavbarHomepage />
       <Header />
       <CardDescription />
+      <Faq />
       <CardInterest />
       <Footer />
     </>
