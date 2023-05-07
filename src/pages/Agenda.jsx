bfa5158a041d4e794b9808aef542233df0958d0c
Safira Tyas Wandita <safeeratyas@gmail.com>
2023-05-07 17:04:25 +0700

    config: add integration who-am-i for agenda page

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -1,11 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCitizenLogin } from "../redux/actions/getCitizenLogin";
+
 import NavbarPendataan from "../components/Navbar/NavbarPendataan";
 import Activity from "../components/Carousel/Activity";
 import CardAgenda from "../components/Cards/Description/CardAgenda";
 
 function Agenda() {
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState([]);
+
+  const {
+    citizenDataResult,
+  } = useSelector((state) => state.getCitizenLoginReducer);
+
+  useEffect(() => {
+    dispatch(getCitizenLogin());
+  }, []);
+
+  useEffect(() => {
+    if (citizenDataResult) {
+      setUserData(citizenDataResult);
+    }
+  }, [citizenDataResult]);
+
   return (
     <>
-      <NavbarPendataan />
+      <NavbarPendataan userData={userData} />
       <Activity />
       <CardAgenda />
     </>
