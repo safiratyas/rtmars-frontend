4f2b7a1a9b06529b378659119ca7956eabaab2e2
Safira Tyas Wandita <safeeratyas@gmail.com>
2023-05-14 16:22:30 +0700

    config: add integration of citizen login in navbar

--- src/pages/FormData.jsx
+++ src/pages/FormData.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getCitizenLogin } from "../redux/actions/getCitizenLogin";
+import { useDispatch, useSelector } from "react-redux";
 import NavbarPendataan from "../components/Navbar/NavbarPendataan";
 import FormRequest from "../components/Form/Request/FormData";
 
 function FormData() {
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState([]);
+
+  const {
+    citizenDataResult,
+  } = useSelector((state) => state.getCitizenLoginReducer);
+
+  useEffect(() => {
+    dispatch(getCitizenLogin());
+  }, []);
+
+  useEffect(() => {
+    if (citizenDataResult) {
+      setUserData(citizenDataResult);
+    }
+  }, [citizenDataResult]);
+
   return (
     <>
-      <NavbarPendataan />
+      <NavbarPendataan userData={userData} />
       <FormRequest />
     </>
   )
